06d3de1aebe8bb47dae7bad2ec57077c54ade069
ivanianonita <42725307+ivanianonita@users.noreply.github.com>
2018-09-10 18:30:59 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,42 @@
 <?php
 
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
-}
+$h=0;
 rekursif(5);
+function rekursif2($n,$bintang){
+global $h;
+	if($n>$h-$bintang){
+		echo "*";
+	rekursif2($n-1,$bintang);
+	}
+}
+function rekursif($bintang){
+	global $h;
+	rekursif2($h,$bintang);
+	echo"<br>";
+	if(--$bintang > $h){
+		rekursif($bintang);
+	}
+}
+
+
+echo"<br>/*================================================================<br><br>";
+
+$angka=5;
+function rekursif3($k,$j,$angka){
+	if($k<=$angka){
+		if($j<=$k){
+			echo $j;
+			$j++;
+			rekursif3($k,$j,$angka);
+
+			}
+			else{
+				echo"<br>";
+				$k++;
+				rekursif3($k,1,$angka);
+			}
+
+		}
+	}
+	rekursif3(1,1,$angka);
 ?>
